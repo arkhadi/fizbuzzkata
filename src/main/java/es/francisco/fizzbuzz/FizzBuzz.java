175882c05a095dc2da2cac4f46ec8bc635bd4a7a
Francisco Fernandez <arkhadi@gmail.com>
2019-05-17 20:40:37 +0100

    Second Cycle

--- src/main/java/es/francisco/fizzbuzz/FizzBuzz.java
+++ src/main/java/es/francisco/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,7 @@ package es.francisco.fizzbuzz;
 public class FizzBuzz {
 
     public String convert(int number) {
+        if(number ==2) return "2";
         return "1";
     }
 }
--- src/test/java/es/francisco/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/es/francisco/fizzbuzz/FizzBuzzTest.java
@@ -7,7 +7,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FizzBuzzTest {
 
     @Test
-    public void ShouldConvertOneToOne() {
+    public void shouldConvertOneToOne() {
         assertThat(new FizzBuzz().convert(1)).isEqualTo("1");
+    }
+
+    @Test
+    void shouldConvertTwoToTwo() {
+        assertThat(new FizzBuzz().convert(2)).isEqualTo("2");
     }
 }
