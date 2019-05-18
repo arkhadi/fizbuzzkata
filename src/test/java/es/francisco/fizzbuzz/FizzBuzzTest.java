13a335210a41851290bdc84c702ee5afc0accd5c
Francisco Fernandez <arkhadi@gmail.com>
2019-05-18 11:34:50 +0100

    Fifth Cycle

--- src/test/java/es/francisco/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/es/francisco/fizzbuzz/FizzBuzzTest.java
@@ -1,9 +1,9 @@
 package es.francisco.fizzbuzz;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,14 +15,16 @@ public class FizzBuzzTest {
     void setUp() {
         classToTest = new FizzBuzz();
     }
+
     @ParameterizedTest
-    @CsvSource({"1,1", "2,2", "4,4"})
+    @CsvSource({"1,1", "2,2", "4,4", "7,7", "8,8", "13,13", "14,14"})
     public void shouldConvertNumberToFizzBuzzString(int input, String expectedValue) {
         assertThat(classToTest.convert(input)).isEqualTo(expectedValue);
     }
 
-    @Test
-    public void convertThreeToFizz() {
-        assertThat(classToTest.convert(3)).isEqualTo("Fizz");
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9, 12, 15})
+    public void convertThreeToFizz(int input) {
+        assertThat(classToTest.convert(input)).isEqualTo("Fizz");
     }
 }
